a578a1eae68650b3803970f664013a11766f45b0
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-09-23 14:02:06 +0300

    qq

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,6 +2,6 @@ import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 export const isValidId = (req, res, next) => {
   const { id } = req.params;
-  if (!isValidObjectId(id)) return next(createHttpError(404, 'Not found11'));
+  if (!isValidObjectId(id)) return next(createHttpError(404, 'Not found'));
   next();
 };
